538c87641dedc522aaf8329b6490d3d31dc77162
zolangi <zolangiramirez@gmail.com>
2017-12-13 20:13:43 -0800

    Add Files

---
+++ questions.js
@@ -0,0 +1,211 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    "QUESTIONS_EN_US": [
+        {
+            "How many minutes to Van Dort's wedding rehearsal?":[
+                "10 minutes",
+                "5 minutes",
+                "15 minutes",
+                "20 minutes",
+            ]
+        },
+        {
+            "What must go according to plan?" :[
+                "Every single, tiny, microscopic little thing",
+                "Every last thing",
+                "Every single thing",
+                "Everything"
+            ]
+        },
+        {
+            "What is the Van Dort's occupation?":[
+                "Fish merchants",
+                "Butchers",
+                "Painters",
+                "Art merchants"
+            ]
+        },
+        {
+            "What kind of wedding do the Everglot's think it is?":[
+                "Ominous",
+                "Deadly",
+                "Fast",
+                "Great"
+            ]
+        },
+        {
+            "What animal do the Everglot's compare their daughter's face to?":[
+                "Otter",
+                "Bear",
+                "Dog",
+                "Cat"
+            ]
+        },
+        {
+            "Who is getting married?":[
+                "Victor and Victoria",
+                "Vincent and Victoria",
+                "Victor and Valerie",
+                "Vincent and Valerie"
+            ]
+        },
+        {
+            "Where do the Everglot's and Van Dort's take their tea?":[
+                "West Drawing room",
+                "Foyer",
+                "Dining table",
+                "Courtyard"
+            ]
+        },
+        {
+            "What is the Everglot's occupation?":[
+                "Aristocrats",
+                "Artists",
+                "Fish Merchants",
+                "Bakers"
+            ]
+        },
+        {
+            "What is Victor's dog's name?":[
+                "Scraps",
+                "Spike",
+                "Sam",
+                "Sally"
+            ]
+        },
+        {
+            "Who does Victor marry instead of Victoria?":[
+                "Emily",
+                "Emilia",
+                "Valerie",
+                "Sally"
+            ]
+        },
+        {
+            "What does Emily give Victor as a wedding present?":[
+                "Dead Dog",
+                "Dead Cat",
+                "Cat",
+                "Dog"
+            ]
+        },
+        {
+            "Who do the Everglot's want Victoria to marry instead of Victor?":[
+                "Lord Barkis",
+                "Lord Marcus",
+                "Lord Farris",
+                "Lord Jarris"
+            ]
+        },
+        {
+            "In order for his marriage to Emily to be valid, what does Victor have to do?":[
+                "Repeat his vows in the Land of the Living and drink the Wine of Ages",
+                "Repeat his vows in the Land of the Dead and drink the Wine of Ages",
+                "Drink the wine of Ages",
+                "Remarry Emily",
+            ]
+        },
+        {
+            "What does Emily transform to as she steps into the moonlight?":[
+                "Butterflies",
+                "Dust",
+                "Worms",
+                "Moths",
+            ]
+        },
+        {
+            "Barkis mockingly proposes a toast to Emily, what does he say to her?":[
+                "always the bridesmaid, never the bride",
+                "always the bridesmaid",
+                "always the bride",
+                "always the dead"
+            ]
+        },
+        {
+            "What does Lord Barkis drink at the church?":[
+                "A cup of poison",
+                "A cup of wine",
+                "A cup of alcohol",
+                "A cup of water"
+            ]
+        },
+        {
+            "What is Victor good at?":[
+                "piano",
+                "saxophone",
+                "guitar",
+                "cello"
+            ]
+        },
+        {
+            "What does Emily have a passion for?":[
+                "Music and Dance",
+                "Theater and Dance",
+                "Theater and Music",
+                "Theater and Art"
+            ]
+        },
+        {
+            "What is the name of the Van Dort's coachman?":[
+                "Mayhew",
+                "Matthew",
+                "Paul",
+                "John",
+            ]
+        },
+        {
+            "What does Emily have in her eye?":[
+                "maggot",
+                "cockroach",
+                "flies",
+                "spider"
+            ]
+        },
+        {
+            "What color is the maggot in Emily's eye?":[
+                "green",
+                "orange",
+                "blue",
+                "red"
+            ]
+        },
+        {
+            "What is the name of the skeleton with the sword stuck in his chest?":[
+                "General Bonesapart",
+                "General Boneapetit",
+                "General Bonejangles",
+                "General Boneavery"
+            ]
+        },
+        {
+            "Why does Victoria's mother not let her near the piano?":[
+                "Music is improper for a young lady",
+                "she'll get the piano dirty",
+                "she'll get dirty from using the piano",
+                "Victoria's mom does not like music"
+            ]
+        },
+        {
+            "Who did Victoria dream of marrying?":[
+                "All of these were her dream",
+                "Someone she loved",
+                "Someone to spend the rest of her life with",
+                "Someone she was deeply in love with"
+            ]
+        },
+        {
+            "When does Lord Barkis arrive for the wedding?":[
+                "Wedding Rehearsals",
+                "During the Wedding",
+                "After the Wedding",
+                "After the wedding rehearsal"
+            ]
+        }
+
+    ]
+};
